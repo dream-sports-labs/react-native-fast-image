37bbbfb6aa4604c37ee5ab728dd8c5b7a2212895
Deepanshu.shukla <deepanshu.shukla@dream11.com>
2024-07-15 15:16:41 +0530

    fix: test commit

--- ReactNativeFastImageExample/index.js
+++ ReactNativeFastImageExample/index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src';
 import {name as appName} from './app.json';
-
+console.log('this is test commit');
 AppRegistry.registerComponent(appName, () => App);
