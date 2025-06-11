34604cf4a88a759e59c7a40a70aa9f1abd9393d3
dream11-atharva <atharva.kothawade@dream11.com>
2025-06-11 17:14:47 +0530

    chore : added method in interface

--- src/NativeFastImageView.ts
+++ src/NativeFastImageView.ts
@@ -1,15 +1,14 @@
 import type { TurboModule } from 'react-native'
 import { TurboModuleRegistry } from 'react-native'
-import {  DiskCacheSize, Source } from './index'
+import { DiskCacheSize, Source } from './index'
 
 
 
 export interface Spec extends TurboModule {
-    readonly getConstants: () => {}
-    readonly preload: (sources: Source[]) => void
-    readonly clearMemoryCache: () => Promise<void>
-    readonly clearDiskCache: () => Promise<void>
-    readonly getDiskCacheSize: () => Promise<DiskCacheSize>
+    preload: (sources: Source[]) => void
+    clearMemoryCache: () => Promise<void>
+    clearDiskCache: () => Promise<void>
+    getDiskCacheSize: () => Promise<DiskCacheSize>
 }
 
 export default TurboModuleRegistry.get<Spec>('FastImageView')
